ff381aa9cdb72a9858a9705cc79c61696c52ea90
潘林海 <>
2020-06-04 22:46:07 +0800

    update

---
+++ PLShopCart/Pods/Target Support Files/Pods-PLShopCart/Pods-PLShopCart-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PLShopCart : NSObject
+@end
+@implementation PodsDummy_Pods_PLShopCart
+@end
---
+++ PLShopCart/Pods/Target Support Files/Pods-PLShopCartTests/Pods-PLShopCartTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PLShopCartTests : NSObject
+@end
+@implementation PodsDummy_Pods_PLShopCartTests
+@end
